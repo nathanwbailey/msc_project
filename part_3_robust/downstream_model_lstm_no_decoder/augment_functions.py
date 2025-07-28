732c8bc8f535c3f93641ed19726b05bd64a2379c
nb324 <nb324@ic.ac.uk>
2025-07-28 16:39:29 +0100

    Small Clean to file

--- part_3_robust/downstream_model_lstm_no_decoder/augment_functions.py
+++
@@ -1,8 +0,0 @@
-import torch
-
-
-def random_mask(sample, mask_prob=0.7):
-    random_tensor = torch.rand(sample.shape, device=sample.device)
-    mask = (random_tensor > mask_prob).float()
-    masked_image = sample * mask
-    return masked_image
